ee18b969600be39a6e320ca77d9f90343b2752e9
Sophie Digal <you@example.com>
2019-11-22 18:59:15 -0800

    Updates

---
+++ precache-manifest.f7c173cee800f2a2bd4aec4614f9d781.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0ad4f20b5bbcbf775136cc42658e7a8",
+    "url": "/roboFriends/index.html"
+  },
+  {
+    "revision": "10ca8a264ab078a444e0",
+    "url": "/roboFriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "34e573e14ab09d5b8135",
+    "url": "/roboFriends/static/css/main.321db667.chunk.css"
+  },
+  {
+    "revision": "10ca8a264ab078a444e0",
+    "url": "/roboFriends/static/js/2.49b130ef.chunk.js"
+  },
+  {
+    "revision": "34e573e14ab09d5b8135",
+    "url": "/roboFriends/static/js/main.ae366cdc.chunk.js"
+  },
+  {
+    "revision": "ec118206ed1625b5d5f4",
+    "url": "/roboFriends/static/js/runtime-main.ce3546fb.js"
+  }
+]);
